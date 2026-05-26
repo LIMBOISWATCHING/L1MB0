a2a38beb79bc39b6907734f23e762ba3e09ef257
LIMBOISWATCHING <158734318+LIMBOISWATCHING@users.noreply.github.com>
2026-05-26 15:45:49 -0300

    Add files via upload

---
+++ ChatOn/script.js
@@ -0,0 +1,188 @@
+import { initializeApp }
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+
+  getDatabase,
+  ref,
+  push,
+  onChildAdded,
+  set,
+  onValue,
+  remove
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDU86YIxOEF4PniyquLKQJoNckxhkKIXFI",
+
+  authDomain: "chaton-3ee70.firebaseapp.com",
+
+  databaseURL: "https://chaton-3ee70-default-rtdb.firebaseio.com/",
+
+  projectId: "chaton-3ee70",
+
+  storageBucket: "chaton-3ee70.firebasestorage.app",
+
+  messagingSenderId: "725824098805",
+
+  appId: "1:725824098805:web:636f25ab9360de14d050bc"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+const mensagensRef = ref(db, "mensagens");
+
+const onlineRef = ref(db, "online");
+
+const chat = document.getElementById("chat");
+
+const form = document.getElementById("form");
+
+const input = document.getElementById("input");
+
+const online = document.getElementById("online");
+
+const nickInput = document.getElementById("nickInput");
+
+const entrar = document.getElementById("entrar");
+
+const nomes = [
+
+  "Ghost",
+  "Void",
+  "Luna",
+  "Echo",
+  "Neko",
+  "Shade",
+  "Zero"
+
+];
+
+let nome = "";
+
+entrar.onclick = () => {
+
+  nome = nickInput.value.trim();
+
+  if(!nome){
+
+    nome =
+      nomes[Math.floor(Math.random() * nomes.length)] +
+      Math.floor(Math.random() * 999);
+
+  }
+
+  document.getElementById("nickBox")
+  .style.display = "none";
+
+};
+
+const userId =
+  Math.random().toString(36).substring(2);
+
+set(ref(db, "online/" + userId), true);
+
+window.addEventListener("beforeunload", () => {
+
+  remove(ref(db, "online/" + userId));
+
+});
+
+onValue(onlineRef, (snapshot) => {
+
+  const total = snapshot.size || 0;
+
+  online.innerText = total + " online";
+
+});
+
+function criarMensagem(dados){
+
+  const div = document.createElement("div");
+
+  div.classList.add("msg");
+
+  div.innerHTML = `
+    <span class="nome">${dados.nome}:</span>
+    ${dados.texto}
+  `;
+
+  chat.appendChild(div);
+
+  chat.scrollTop = chat.scrollHeight;
+
+  setTimeout(() => {
+
+    div.classList.add("sumindo");
+
+  }, 50000);
+
+  setTimeout(() => {
+
+    div.remove();
+
+  }, 52000);
+
+}
+
+onChildAdded(mensagensRef, (snapshot) => {
+
+  criarMensagem(snapshot.val());
+
+});
+
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  if(!nome){
+
+    alert("Escolha um nick primeiro");
+
+    return;
+
+  }
+
+  const texto = input.value.trim();
+
+  if(!texto) return;
+
+  push(mensagensRef, {
+
+    nome,
+    texto,
+    tempo: Date.now()
+
+  });
+
+  input.value = "";
+
+});
+
+setInterval(() => {
+
+  onValue(mensagensRef, (snapshot) => {
+
+    snapshot.forEach((msg) => {
+
+      const dados = msg.val();
+
+      if(Date.now() - dados.tempo > 60000){
+
+        remove(ref(db, "mensagens/" + msg.key));
+
+      }
+
+    });
+
+  });
+
+}, 5000);
